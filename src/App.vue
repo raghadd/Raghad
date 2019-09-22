2ac28dcc13b77faa02f83e465040cfcf682640c6
Raghad Alafeef <raghadalafeef@gmail.com>
2019-09-23 00:15:59 +0300

    header kinda in place but still needs resizing options

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/determined.png">
-    <HelloWorld msg="Raghad Alafeef "/>
+    <div class="container">
+
+      <div class="logo-div">
+        <img alt="Vue logo" src="./assets/determined.png">
+      </div>
+
+      <div class="menu-div">
+        <RaghadMenu msg="Raghad Alafeef " /> 
+      </div>
+
+      <div class="content-div">
+        <works msg="Raghad Alafeef"/>
+      </div>
+      
+      <!-- logo="../assets/determined.png" -->
+      
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import works from './components/works.vue'
+import RaghadMenu from './components/RaghadMenu.vue'
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    works,
+    RaghadMenu
+  },
+  data () {
+    return {
+
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Lucida Console", "Lucida Sans Typewriter", monaco, "Bitstream Vera Sans Mono", monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+}
+
+.container {
+  width: 100%;
+  padding: 0 10px;
+  height: auto;
+}
+.logo-div {
+  width: 15%;
+  float: left;
+}
+.menu-div {
+  margin-left: 17%;
+  height: 90px;
+}
+.content-div {
+  margin-top: 5%;
+  height: 100%;
+
+}
+
+.logo-div img {
+  max-width: 70%;
 }
 </style>
